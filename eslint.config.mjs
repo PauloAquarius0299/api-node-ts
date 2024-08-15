fb88ea19708a1fb5cc20b0e131ac06c11a7daa86
Paulo Cesar <paulocgsantos99@gmail.com>
2024-08-15 15:05:47 -0300

    feat: add getuserscontrollers

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,29 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    languageOptions: {
+      parser: tsParser,
+      globals: globals.browser,
+    },
+    plugins: {
+      "@typescript-eslint": tseslint,
+    },
+    rules: {
+      "@typescript-eslint/no-explicit-any": "error", // Para evitar o uso de 'any'
+      // Outras regras podem ser adicionadas aqui
+    },
+  },
+  {
+    files: ["**/*.js"],
+    ...pluginJs.configs.recommended,
+  },
+  {
+    files: ["**/*.ts"],
+    ...tseslint.configs.recommended,
+  },
 ];
