bd08323b66e3de76b362ef97d6a9fd573245e276
Paulo Cesar <paulocgsantos99@gmail.com>
2024-08-18 17:12:25 -0300

    feat: add MongoGetUserRepository

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,22 @@
 import express from 'express'
 import {config} from 'dotenv';
+import { GetUsersController } from './controllers/get-users/get-user';
+import {MongoGetUsersRepository} from './repositories/get-users/mongo-get-users'
 
 config();
 
 const app = express();
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 3000;
+
+app.get("/users", async (req, res) => {
+    const mongoGetUsersRepository = new MongoGetUsersRepository();
+
+    const getUsersController = new GetUsersController(mongoGetUsersRepository);
+
+    const {body, statusCode} = await getUsersController.handle();
+
+    res.send(body).status(statusCode);
+});
 
 app.listen(port, () => console.log(`listening on port ${port}!`));
-
-app.get("/", (req, res) => {
-    res.send('hello world');
-});
